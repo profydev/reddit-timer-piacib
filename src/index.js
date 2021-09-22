5e61550052b376664f9c3af8f53d9c61c30d308c
piacib <ben.piacitelli@gmail.com>
2021-09-22 12:39:28 -0700

    moved router from app to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './normalize.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'),
+);
