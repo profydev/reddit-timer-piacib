3c9e30c1331394659c13509f479f2965ee23ad08
piacib <ben.piacitelli@gmail.com>
2021-09-22 12:44:00 -0700

    initial layout of search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Table from './Table';
 
-const Search = () => (
-  <div>
-    <Link to="/search" />
-    <Link to="/#howitworks" />
-    <Link to="/#about" />
-  </div>
-);
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+align-items: center`;
+const FormContainer = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`;
+const Headline = styled.h1`
+  color: ${(props) => props.theme.color.dark};
+  font-size: 38px; 
+  line-height: 46px;
+  letter-spacing: .9px;
+  font-style: normal;
+font-weight: normal;
+  /* ${(props) => props.theme.font.size.headline}; */
+`;
+const SubmitSubredditSearch = styled.input`
+  background: rgba(253,183,85,1);
+  border:none;
+  border-radius: 4px;
+  font-size: ${(props) => props.theme.font.size.small};
+  color: ${(props) => props.theme.color.light};
+  width:13.5625rem;
+  
+  padding: 10px 0;
+`;
+const RLabel = styled.label`
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+  color:${(props) => props.theme.color.text};
+  
+`;
+const SubredditInput = styled.input`border:none;
+color:inherit;
+width:fit-content;
+`;
+const Search = () => {
+  const [subredditInput, setSubredditInput] = useState('javascript');
+  const handleSubredditInputChange = (event) => setSubredditInput(event.target.value);
+  const handleSubredditSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <SearchContainer>
+      <Headline>No reactions to your reddit posts?</Headline>
+      <h3>Great timing, great results! Find the best time to post on your subreddit.</h3>
+      <FormContainer onSubmit={handleSubredditSubmit}>
+        <SubmitSubredditSearch type="submit" value="SHOW ME THE BEST TIME" />
+        <RLabel htmlFor="subreddit">
+          r/
+          <SubredditInput name="subreddit" value={subredditInput} type="text" onChange={handleSubredditInputChange} />
+        </RLabel>
+      </FormContainer>
+      <Table subreddit=" " />
+    </SearchContainer>
+  );
+};
 
 export default Search;
