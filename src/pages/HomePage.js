b0737a1c0c9951e92e4895b5b2732f43a1ca77f7
piacib <ben.piacitelli@gmail.com>
2021-09-22 12:41:38 -0700

    removed footer and header

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import Table from '../components/Table';
 import Search from '../components/Search';
 import Posts from '../components/Posts';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
 
 const HomePage = () => (
   <div>
-    <Header />
-    <Table />
     <Search />
     <Posts />
-    <Footer />
   </div>
 );
 
