380d5cb6370dfb8ab06caf82844dc484e4cc2074
piacib <ben.piacitelli@gmail.com>
2021-09-22 12:44:52 -0700

    added theme values for color table search and container sizes

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,19 @@
 const theme = {
   color: {
-    text: 'rgba(147, 145, 143, 1)',
+    dark: 'rgba(0,0,0,1)', // #000000
+    text: 'rgba(147,145,143,1)', // #93918f
+    midDark: 'rgba(99,99,99,1)', // #636363
+    midLight: 'rgba(213,213,213,1)', // #d5d5d5
+    light: 'rgba(255,255,255,1)', // #ffffff
+  },
+  backgroundColor: {
+    tableDay: 'rgba(30,37,55,1)',
+  },
+  table: {
+    boxSize: '40px',
+    tableHeadHeight: '52px',
+    tableHeadBackground: 'linear-gradient(180deg, #FEFEFE 0%, #E9E9E9 100%);',
+    dateContainerWidth: '154px',
   },
   font: {
     family: {
@@ -9,10 +22,16 @@ const theme = {
     },
     size: {
       default: '16px',
+      headline: '2.375rem',
+      small: '0.875rem',
     },
     lineHeight: {
       default: 1.69,
     },
+  },
+  containerSizes: {
+    headerHeight: '100px',
+    footerHeight: '100px',
   },
 };
 
