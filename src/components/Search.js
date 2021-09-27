834ded60ae3fdd2742fcf71b5ddaf7b0a8da0faa
piacib <ben.piacitelli@gmail.com>
2021-09-27 16:53:55 -0700

    added useParams and made subreddit input readonly on homepage

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Table from './Table';
 
 const SearchContainer = styled.div`
   display: flex;
   flex-direction: column;
-align-items: center`;
+  align-items: center
+`;
 const Headline = styled.h1`
   color: ${(props) => props.theme.color.dark};
   font-size: 38px; 
@@ -14,7 +15,7 @@ const Headline = styled.h1`
   line-height: 46px;
   letter-spacing: .9px;
   font-style: normal;
-font-weight: normal;
+  font-weight: normal;
   /* ${(props) => props.theme.font.size.headline}; */
 `;
 const SubmitSubredditSearch = styled(Link)`
@@ -25,25 +26,50 @@ const SubmitSubredditSearch = styled(Link)`
   border-radius: 4px;
   font-size: ${(props) => props.theme.font.size.small};
   color: ${(props) => props.theme.color.light};
-  width:13.5625rem;
-  
+  width:${(props) => props.theme.showBestResults.width};
   padding: 0.375rem 0;
 `;
-const SubredditText = styled.p`
-  margin: 2.625rem 0  0 0;
+const SubredditInput = styled.input`
+  border: none;
+  font-family: 'Montserrat', sans-serif;
+
+  width: ${(props) => `calc(${props.theme.showBestResults.width} / 2 )`};
+  &:focus{
+    outline:${(props) => (props.readOnly ? 'none' : 'default')};
+  }
 `;
 const SubText = styled.p`
   margin:1rem 0 0;
   `;
+const SubredditInputContainer = styled.div`
+  margin: 2.625rem 0 0;
+  display: flex;
+  justify-content: center;
+  overflow: visible;
+  width: ${(props) => props.theme.showBestResults.width};
+
+  `;
+const SubredditInputLabel = styled.label`
+ padding-left:2rem;
+  text-align: end;
+`;
 const Search = () => {
-  const [subredditInput] = useState('javascript');
+  const { subreddit } = useParams();
+  const [searchParam, setSearchParam] = useState('javascript');
+  const page = subreddit ? false : 'readOnly';
+  const handleChange = (e) => {
+    setSearchParam(e.target.value);
+  };
   return (
     <SearchContainer>
       <Headline>No reactions to your reddit posts?</Headline>
       <SubText>Great timing, great results! Find the best time to post on your subreddit.</SubText>
-      <SubmitSubredditSearch to="/search/javascript">SHOW ME THE BEST TIME</SubmitSubredditSearch>
-      <SubredditText>r/javascript</SubredditText>
-      <Table subreddit={subredditInput} />
+      <SubmitSubredditSearch to={`/search/${searchParam}`}>SHOW ME THE BEST TIME</SubmitSubredditSearch>
+      <SubredditInputContainer>
+        <SubredditInputLabel>r/</SubredditInputLabel>
+        <SubredditInput type="text" placeHolder="r/" value={searchParam} readOnly={page} onChange={handleChange} />
+      </SubredditInputContainer>
+      <Table subreddit={subreddit} />
     </SearchContainer>
   );
 };
