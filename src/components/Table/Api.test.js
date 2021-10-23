9144b5a86b00e85cbd5a7cf166e791f89c69c082
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-10-23 10:56:36 -0700

    moved Table to seperate folder

---
+++ src/components/Table/Api.test.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { MemoryRouter } from 'react-router-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Table from './Table';
+import '@testing-library/jest-dom';
+import 'jest-styled-components';
+import theme from '../../theme';
+import testResponse from './testResponse';
+import App from '../../App';
+
+const server = setupServer(
+  rest.get('https://api.pushshift.io/reddit/submission/search/', async (req, res, ctx) => res(ctx.json(testResponse))),
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+// test('handler server error', () => {
+//   server.use(
+//     rest.get(url, (req, res, ctx) => res(ctx.status(500))),
+//   );
+//   render(
+//     <MemoryRouter>
+//       <ThemeProvider theme={theme}>
+//         <Table subreddit="javascript" />
+//       </ThemeProvider>
+//     </MemoryRouter>,
+//   screen.debug(screen.getAllByRole(/table/i));
+//   );
+// });
+test('when no subreddit is provided table img appears', async () => {
+  render(
+    <MemoryRouter>
+      <ThemeProvider theme={theme}>
+        <Table subreddit="" />
+      </ThemeProvider>
+    </MemoryRouter>,
+  );
+  expect(screen.getByAltText(/heat map/i));
+});
+test('when a subreddit is provided table appears', async () => {
+  render(
+    <MemoryRouter>
+      <ThemeProvider theme={theme}>
+        <Table subreddit="javascript" />
+      </ThemeProvider>
+    </MemoryRouter>,
+  );
+  expect(screen.getByRole(/table/i));
+});
+test('when a subreddit is provided table data is displayed', async () => {
+  render(
+    <MemoryRouter initialEntries={['/']} initialIndex={0}>
+      <App />
+    </MemoryRouter>,
+  );
+  const link = screen.getByTestId('reddit-timer-svg');
+  userEvent.click(link);
+  const showMeLink = screen.getByRole('link', { name: /SHOW ME THE BEST TIME/i });
+  userEvent.click(showMeLink);
+  expect(window.location.href).toContain('search/javascript');
+  await waitForElementToBeRemoved(screen.getByAltText(/loading spinner/i));
+  expect(screen.getAllByText('1')).toBeTruthy();
+  expect(screen.getAllByText('2')).toBeTruthy();
+  expect(screen.getAllByText('3')).toBeTruthy();
+});
---
+++ src/components/Table/Table.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import SelectedPostDisplay from '../Posts/SelectedPostsDisplay';
+import tableImg from './media/table.png';
+import Loading from '../Loading/Loading';
+import {
+  generateHours,
+  generateUrl,
+  updateCalendar,
+  defaultCalendar,
+} from './TableFunctions';
+import {
+  TableContainer,
+  TableHead,
+  TimeDisplay,
+  DateContainer,
+  DateDisplay,
+  HeatmapTable,
+  HeatmapTableBody,
+  HeatmapTableRow,
+  HeatMapDataEntry, SelectedPostContainer,
+} from './TableStyles';
+
+const dates = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const times = ['12:00am', '2:00am', '4:00am', '6:00am', '8:00am', '10:00am', '12:00pm', '2:00pm', '4:00pm', '6:00pm', '8:00pm', '10:00pm'];
+const Table = ({ subreddit }) => {
+  const [calendar, setCalendar] = useState(defaultCalendar);
+  const [posts, setPosts] = useState([]);
+  const [selectedPost, setSelectedPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    // early exit function
+    if (!subreddit) {
+      return;
+    }
+    const redditUrl = generateUrl({ timeFrame: 'after=1y', subreddit, querySize: 100 });
+    async function fetchSubredditData(url) {
+      // reset calendar data while
+      // new data is fetched
+      setLoading(true);
+      setCalendar(defaultCalendar);
+      // fetch data
+      const response = await fetch(url);
+      const jsonResp = await response.json();
+      const postsArray = [];
+      jsonResp.data.forEach((post) => {
+        const date = new Date(post.created_utc * 1000);
+        postsArray.push({
+          score: post.score,
+          title: post.title,
+          upvote_ratio: post.upvote_ratio,
+          full_link: post.full_link,
+          author: post.author,
+          created_utc: post.created_utc,
+          created_day: date.getDay(),
+          created_hour: generateHours(date),
+        });
+      });
+      const updatedCalendar = await updateCalendar(postsArray);
+      // set fetched data to states
+      setPosts(postsArray);
+      setCalendar(updatedCalendar);
+      setLoading(false);
+      return {
+        fetchedPosts: postsArray,
+        fetchedCalendar: updatedCalendar,
+      };
+    }
+    fetchSubredditData(redditUrl);
+
+    // updates when new subreddit is searched
+  }, [subreddit]);
+  return subreddit ? (
+    <>
+      <TableContainer>
+        <Loading isLoading={loading} />
+        <TableHead>
+          {times.map((time) => (
+            <TimeDisplay key={time}>{time}</TimeDisplay>
+          ))}
+        </TableHead>
+        <DateContainer>
+          {dates.map((day) => (
+            <DateDisplay key={day}>{day}</DateDisplay>
+          ))}
+        </DateContainer>
+        <HeatmapTable>
+          <HeatmapTableBody>
+            {Object.keys(calendar).map((dayOfTheWeek) => (
+              <HeatmapTableRow key={dayOfTheWeek}>
+                {Object.keys(calendar[dayOfTheWeek])
+                  .slice(0, 24)
+                  .map((timeOfTheDay) => (
+                    <HeatMapDataEntry
+                      onClick={() => setSelectedPosts(
+                        posts.filter(
+                          (post) => post.created_day === Number(dayOfTheWeek)
+                              && post.created_hour === Number(timeOfTheDay),
+                        ),
+                      )}
+                      key={Math.random()}
+                      value={calendar[dayOfTheWeek][timeOfTheDay]}
+                    >
+                      {calendar[dayOfTheWeek][timeOfTheDay]}
+                    </HeatMapDataEntry>
+                  ))}
+              </HeatmapTableRow>
+            ))}
+          </HeatmapTableBody>
+        </HeatmapTable>
+      </TableContainer>
+      <SelectedPostContainer>
+        {selectedPost.map((post) => (
+          <SelectedPostDisplay
+            title={post.title}
+            score={post.score}
+            upvoteRatio={post.upvote_ratio}
+            fullLink={post.full_link}
+            author={post.author}
+          />
+        ))}
+      </SelectedPostContainer>
+    </>
+  ) : (
+    <TableContainer>
+      <img src={tableImg} alt="heat map table" />
+    </TableContainer>
+  );
+};
+Table.propTypes = {
+  subreddit: PropTypes.string,
+};
+Table.defaultProps = {
+  subreddit: '',
+};
+export default Table;
