0bbbf2ad4518a0768a00b32455954d835ad296b1
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-10-23 11:00:10 -0700

    added Router and themeprovider

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import GlobalStyle from './globalStyles';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import theme from './theme';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const ContentContainer = styled.div`
-  width:100%;
-  height:${(props) => `calc(100vh - ${props.theme.containerSizes.headerHeight} - ${props.theme.containerSizes.footerHeight})`};
+  width: 100%;
+  height: ${(props) => `calc(100vh - ${props.theme.containerSizes.headerHeight} - ${props.theme.containerSizes.footerHeight})`};
   height: fit-content;
-  min-height: 800px;
+  ${'' /* min-height: 800px; */}
 `;
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Header />
-      <ContentContainer>
-        <Switch>
-          <Route path="/search/:subreddit">
-            <SearchPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-          <Route>404 - Not Found</Route>
-        </Switch>
-      </ContentContainer>
-      <Footer />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header />
+        <ContentContainer>
+          <Switch>
+            <Route exact path="/search/:subreddit">
+              <SearchPage />
+            </Route>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route>404 - Not Found</Route>
+          </Switch>
+        </ContentContainer>
+        <Footer />
+      </ThemeProvider>
+    </Router>
   );
 }
 
