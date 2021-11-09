5ea64477c6eff9fee721f9a7e208a224fc83ec03
Ben Piacitelli <ben.piacitelli@gmail.com>
2021-11-08 16:35:15 -0800

    added mobile display

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -8,9 +8,10 @@ import {
   generateUrl,
   updateCalendar,
   defaultCalendar,
+  timeFrames,
 } from './TableFunctions';
 import {
-  TableContainer,
+  TableContainer, HeatMapImage,
   TableHead,
   TimeDisplay,
   DateContainer,
@@ -31,7 +32,7 @@ const Table = ({ subreddit }) => {
   useEffect(() => {
     // early exit function
     if (!subreddit) return {};
-    const redditUrl = generateUrl({ timeFrame: 'after=1y', subreddit, querySize: 100 });
+    const redditUrl = generateUrl({ timeFrame: timeFrames[0], subreddit, querySize: 100 });
     const controller = new AbortController();
     async function fetchSubredditData(url) {
       // reset calendar data while
@@ -125,7 +126,7 @@ const Table = ({ subreddit }) => {
     </>
   ) : (
     <TableContainer>
-      <img src={tableImg} alt="heat map table" />
+      <HeatMapImage src={tableImg} alt="heat map table" />
     </TableContainer>
   );
 };
--- src/components/Table/TableStyles.js
+++ src/components/Table/TableStyles.js
@@ -6,17 +6,38 @@ export const TableContainer = styled.div`
   height: 332px;
   border: none;
   display: grid;
+  position: relative;
   margin: 31px 0 0 0;
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
   grid-template-columns: ${(props) => props.theme.table.dateContainerWidth} auto;
   grid-template-rows: ${(props) => props.theme.table.tableHeadHeight} auto;
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+ transform: rotate(90deg);
+ margin-top: 400px; 
+ margin-bottom: 400px; 
+ ${'' /* display: block; */}
+ 
+ margin-left: ${(props) => props.theme.table.tableHeadHeight};
+ ${'' /* height: 1114px; */}
+ ${'' /* width: 332px; */}
+
+  }
+`;
+export const HeatMapImage = styled.img`
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+    ${'' /* aspect-ratio: 1115∶333;
+    width: 800px;
+    grid-column: 1/3;
+    ${'' /* height: 166px; */}
+    justify-self: center; */}
+  }
 
 `;
+
 export const TableHead = styled.div`
   height: ${(props) => props.theme.table.tableHeadHeight};
   background: ${(props) => props.theme.table.tableHeadBackground};
-  /* width:960px; */
   align-self: end;
   display: flex;
   flex-direction: row;
@@ -30,32 +51,56 @@ export const TimeDisplay = styled.div`
   width: ${(props) => `calc(2 * ${props.theme.table.boxSize})`};
 `;
 export const DateContainer = styled.div`
-  display: flex;
+  ${'' /* display: flex; */}
   flex-direction: column;
   justify-content: space-between;
   width: ${(props) => props.theme.table.dateContainerWidth};
   background-color: ${(props) => props.theme.backgroundColor.tableDay};
+
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+ ${'' /* transform: rotate(90deg); */}
+ ${'' /* position: sticky; */}
+ left:-550px;
+ top:0px;
+ z-index: 1;
+
+  }
 `;
 export const DateDisplay = styled.div`
   grid-column: 1;
   grid-row: 2;
   font-size: 0.9375rem;
-  color: ${(props) => props.theme.color.light};
-  width: 100%;
+   color: ${(props) => props.theme.color.light};
+  ${'' /* width: 100%; */}
   height: ${(props) => props.theme.table.boxSize};
   text-align: center;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+    width: 154px;
+    justify-content: center; 
+    font-size: 0.85rem;
+
+${'' /*  */}
+    line-height: 1;
+    writing-mode: vertical-rl;
+    transform: rotate(-90deg);
+    text-orientation: upright;
+ }  
 `;
 export const HeatmapTable = styled.table`
  border-spacing: 0;
+  ${'' /* @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {
+    transform: rotate(90deg);
+  } */}
 `;
 export const HeatmapTableBody = styled.tbody`
   border-spacing: 0;
   grid-column: 2;
   grid-row: 2;
+  
 `;
 export const HeatmapTableRow = styled.tr`
   height: ${(props) => props.theme.table.boxSixe};
@@ -68,6 +113,9 @@ export const HeatMapDataEntry = styled.td`
   padding: 0;
   height: 40px;
   width: 40px;
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+    transform: rotate(-90deg);
+  }
 `;
 export const SelectedPostContainer = styled.div`
   /* width:100%; */
