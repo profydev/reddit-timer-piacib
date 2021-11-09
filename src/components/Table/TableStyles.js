d8ad8f8c0d053d2d8e1cbcdeb18f4072887b4b42
piacib <36933979+piacib@users.noreply.github.com>
2021-11-09 08:52:48 -0800

    Hero section (#5)
    * moved router from app to index
    
    * removed Router and moved header and footer out of content container
    
    * removed footer and header
    
    * set up table with dummy data
    
    * moved height to theme
    
    * moved height to theme
    
    * initial layout of search component
    
    * added theme values for color table search and container sizes
    
    * removed css for a tags and moved color to theme
    
    * added color map to heatmap display
    
    * added min height to content container
    
    * added link to search element
    
    * added letter spacing prop
    
    * added letter spacing prop
    
    * fixed padding and fonts for components
    
    * added async fetch function and data display
    
    * added async fetch function and data display
    
    * made search page route take subreddit param
    
    * added showBEstResultswidth
    
    * added img of table
    
    * set default input font
    
    * added prop types
    
    * added useParams and made subreddit input readonly on homepage
    
    * removed unnecessary div
    
    * removed unnecessary div
    
    * added component to display posts from day/time
    
    * cypress updated
    
    * added testing packages
    
    * moved footer to seperate folder'
    
    * moved header to seperate folder
    
    * moved Posts to seperate folder
    
    * moved Search to seperate folder
    
    * moved Table to seperate folder
    
    * moved Loading to seperate folder
    
    * moved tests to component folders
    
    * moved Router to App
    
    * added Router and themeprovider
    
    * seperated tests into component specific tests
    
    * moved components to folders
    
    * moved to component folders
    
    * rearranged page layout
    
    * testing packages added
    
    * test folder deleted
    
    * formatting fixed
    
    * added cleanup function and catch statement to useEffect
    
    * removed unnecessary comment
    
    * removed console log statement
    
    * removed error state
    
    * added mobile display
    
    * made time display date flip
    
    * added mobile table values
    
    * added mobile table values
    
    * added timeFrames array

--- src/components/Table/TableStyles.js
+++ src/components/Table/TableStyles.js
@@ -6,48 +6,93 @@ export const TableContainer = styled.div`
   height: 332px;
   border: none;
   display: grid;
+  position: relative;
   margin: 31px 0 0 0;
   font-family: 'Montserrat', sans-serif;
   font-weight: 600;
   grid-template-columns: ${(props) => props.theme.table.dateContainerWidth} auto;
   grid-template-rows: ${(props) => props.theme.table.tableHeadHeight} auto;
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+ transform: rotate(90deg);
+ margin-top: 400px; 
+ margin-bottom: 400px; 
+ ${'' /* display: block; */}
+ 
+ margin-left: ${(props) => props.theme.table.tableHeadHeight};
+ ${'' /* height: 1114px; */}
+ ${'' /* width: 332px; */}
+
+  }
+`;
+export const HeatMapImage = styled.img`
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+    ${'' /* aspect-ratio: 1115∶333;
+    width: 800px;
+    grid-column: 1/3;
+    ${'' /* height: 166px; */}
+    justify-self: center; */}
+  }
 
 `;
+
 export const TableHead = styled.div`
   height: ${(props) => props.theme.table.tableHeadHeight};
   background: ${(props) => props.theme.table.tableHeadBackground};
-  /* width:960px; */
   align-self: end;
   display: flex;
   flex-direction: row;
   align-items: center;
   grid-column: 2;
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+    height: 4.5rem;
+  }
 `;
 export const TimeDisplay = styled.div`
   text-align: center;
   font-size: 0.875rem;
   font-weight: 600;
   width: ${(props) => `calc(2 * ${props.theme.table.boxSize})`};
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+    transform: rotate(-90deg);
+  } 
 `;
 export const DateContainer = styled.div`
-  display: flex;
+  ${'' /* display: flex; */}
+
   flex-direction: column;
   justify-content: space-between;
   width: ${(props) => props.theme.table.dateContainerWidth};
   background-color: ${(props) => props.theme.backgroundColor.tableDay};
+
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+ ${'' /* transform: rotate(90deg); */}
+ ${'' /* position: sticky; */}
+ left:-550px;
+ top:0px;
+ z-index: 1;
+
+  }
 `;
 export const DateDisplay = styled.div`
   grid-column: 1;
   grid-row: 2;
   font-size: 0.9375rem;
-  color: ${(props) => props.theme.color.light};
-  width: 100%;
-  height: ${(props) => props.theme.table.boxSize};
   text-align: center;
+  color: ${(props) => props.theme.color.light};
+  height: ${(props) => props.theme.table.boxSize};
+
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+    width: ${(props) => props.theme.table.dateContainerWidth};
+    font-size: 0.8rem;
+    line-height: 1;
+    writing-mode: vertical-rl;
+    transform: rotate(-90deg);
+    text-orientation: upright;
+ }  
 `;
 export const HeatmapTable = styled.table`
  border-spacing: 0;
@@ -68,6 +113,9 @@ export const HeatMapDataEntry = styled.td`
   padding: 0;
   height: 40px;
   width: 40px;
+  @media (max-width: ${(props) => props.theme.tableMobile.tableMinimizeMaxWidth}) {    
+    transform: rotate(-90deg);
+  }
 `;
 export const SelectedPostContainer = styled.div`
   /* width:100%; */
