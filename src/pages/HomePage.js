e085fff92b2063c005e41ac576c91e7adc2546f4
piacib <ben.piacitelli@gmail.com>
2021-11-19 17:25:16 -0800

    removed unnecessary post component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Search from '../components/Search/Search';
-import Posts from '../components/Posts/Posts';
 
 const HomePage = () => (
   <>
     <Search />
-    <Posts />
   </>
 );
 
