121ccb699766bf34919ae1e3683d14c30e1a5d98
piacib <ben.piacitelli@gmail.com>
2021-11-20 08:10:14 -0800

    added Infosection to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import InfoSection from '../components/InfoSection/InfoSection';
 import Search from '../components/Search/Search';
 
 const HomePage = () => (
   <>
     <Search />
+    <InfoSection />
   </>
 );
 
