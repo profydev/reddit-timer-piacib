2e9d9603081020a95281ebd2d9eb1ed4224cca54
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-03-11 16:32:02 -1000

    moved styles and adjusted their imports

--- src/components/Posts/SelectedPostsDisplay.js
+++ src/components/Posts/SelectedPostsDisplay.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import {
+  SelectedPostTitle,
+  SelectedPostAttribute,
+  SelectedPost,
+} from './SelectedPostsDisplay.styles';
 
-const SelectedPostTitle = styled.a`
-  max-width: 700px;
-  overflow: hidden;
-  white-space: pre-wrap;
-`;
-const SelectedPostAttribute = styled.div``;
-const SelectedPost = styled.div`
-  border: 1px solid black;
-  max-width: 700px;
-`;
 const SelectedPostDisplay = ({
   title, score, upvoteRatio, fullLink, author,
 }) => (
