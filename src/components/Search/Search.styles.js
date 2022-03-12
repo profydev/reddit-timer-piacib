4cea6ea59efeecf01dfe134702bfdfa5991a134b
Ben Piacitelli <ben.piacitelli@gmail.com>
2022-03-11 16:30:27 -1000

    renamed files

---
+++ src/components/Search/Search.styles.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const Headline = styled.h1`
+  color: ${(props) => props.theme.color.dark};
+  font-size: 38px;
+  margin: 1.625rem 0 0;
+  line-height: 46px;
+  letter-spacing: 0.9px;
+  font-style: normal;
+  font-weight: normal;
+  /* ${(props) => props.theme.font.size.headline}; */
+`;
+export const SubmitSubredditSearch = styled(Link)`
+  background: rgba(253, 183, 85, 1);
+  border: none;
+  letter-spacing: 0.07px;
+  margin: 2.6875rem 0 0;
+  border-radius: 4px;
+  text-decoration: none;
+  text-align: center;
+  font-size: ${(props) => props.theme.font.size.small};
+  color: ${(props) => props.theme.color.light};
+  width: ${(props) => props.theme.showBestResults.width};
+  padding: 0.375rem 0;
+`;
+export const SubredditInput = styled.input`
+  border: none;
+  font-family: 'Montserrat', sans-serif;
+
+  width: ${(props) => `calc(${props.theme.showBestResults.width} / 2 )`};
+  &:focus {
+    outline: ${(props) => (props.readOnly ? 'none' : 'default')};
+  }
+`;
+export const SubText = styled.p`
+  margin: 1rem 0 0;
+`;
+export const SubredditInputContainer = styled.div`
+  margin: 2.625rem 0 0;
+  display: flex;
+  justify-content: center;
+  overflow: visible;
+  width: ${(props) => props.theme.showBestResults.width};
+`;
+export const SubredditInputLabel = styled.label`
+  padding-left: 2rem;
+  text-align: end;
+`;
---
+++ src/components/Table/Table.styles.js
@@ -0,0 +1,117 @@
+import styled from 'styled-components';
+import { valueToColor } from './TableFunctions';
+// styled components
+export const TableContainer = styled.div`
+  width: 1114px;
+  height: 332px;
+  border: none;
+  display: grid;
+  position: relative;
+  margin: 31px 0 0 0;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 600;
+  grid-template-columns: ${(props) => props.theme.table.dateContainerWidth} auto;
+  grid-template-rows: ${(props) => props.theme.table.tableHeadHeight} auto;
+  @media (max-width: ${(props) => props.theme.mobile.tableMinimizeMaxWidth}) {    
+ transform: rotate(90deg);
+ margin-top: 400px; 
+ margin-bottom: 400px; 
+ ${'' /* display: block; */}
+ 
+ margin-left: ${(props) => props.theme.table.tableHeadHeight};
+ ${'' /* height: 1114px; */}
+ ${'' /* width: 332px; */}
+
+  }
+`;
+export const HeatMapImage = styled.img`
+  @media (max-width: ${(props) => props.theme.mobile.tableMinimizeMaxWidth}) {    
+  }
+
+`;
+
+export const TableHead = styled.div`
+  height: ${(props) => props.theme.table.tableHeadHeight};
+  background: ${(props) => props.theme.table.tableHeadBackground};
+  align-self: end;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  grid-column: 2;
+  @media (max-width: ${(props) => props.theme.mobile.tableMinimizeMaxWidth}) {    
+    height: 4.5rem;
+  }
+`;
+export const TimeDisplay = styled.div`
+  text-align: center;
+  font-size: 0.875rem;
+  font-weight: 600;
+  width: ${(props) => `calc(2 * ${props.theme.table.boxSize})`};
+  @media (max-width: ${(props) => props.theme.mobile.tableMinimizeMaxWidth}) {    
+
+    transform: rotate(-90deg);
+  } 
+`;
+export const DateContainer = styled.div`
+  ${'' /* display: flex; */}
+  flex-direction: column;
+  justify-content: space-between;
+  width: ${(props) => props.theme.table.dateContainerWidth};
+  background-color: ${(props) => props.theme.backgroundColor.tableDay};
+
+  @media (max-width: ${(props) => props.theme.mobile.tableMinimizeMaxWidth}) {    
+
+ ${'' /* transform: rotate(90deg); */}
+ ${'' /* position: sticky; */}
+ left:-550px;
+ top:0px;
+ z-index: 1;
+
+  }
+`;
+export const DateDisplay = styled.div`
+  grid-column: 1;
+  grid-row: 2;
+  font-size: 0.9375rem;
+  text-align: center;
+  color: ${(props) => props.theme.color.light};
+  height: ${(props) => props.theme.table.boxSize};
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: ${(props) => props.theme.mobile.tableMinimizeMaxWidth}) {    
+    width: ${(props) => props.theme.table.dateContainerWidth};
+    font-size: 0.8rem;
+    line-height: 1;
+    writing-mode: vertical-rl;
+    transform: rotate(-90deg);
+    text-orientation: upright;
+ }  
+`;
+export const HeatmapTable = styled.table`
+ border-spacing: 0;
+`;
+export const HeatmapTableBody = styled.tbody`
+  border-spacing: 0;
+  grid-column: 2;
+  grid-row: 2;
+`;
+export const HeatmapTableRow = styled.tr`
+  height: ${(props) => props.theme.table.boxSixe};
+`;
+export const HeatMapDataEntry = styled.td`
+  text-align: center;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.table.color[valueToColor(props.value)]};
+  color: ${(props) => props.theme.color.light};
+  padding: 0;
+  height: 40px;
+  width: 40px;
+  @media (max-width: ${(props) => props.theme.mobile.tableMinimizeMaxWidth}) {    
+    transform: rotate(-90deg);
+  }
+`;
+export const SelectedPostContainer = styled.div`
+  /* width:100%; */
+`;
