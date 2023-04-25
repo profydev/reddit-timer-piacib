86f00ef188f7e3d51b46db8178f16e3db08fbcc6
piacib <ben.piacitelli@gmail.com>
2023-04-25 12:51:40 -0700

    added margin

--- src/components/Search/Search.styles.js
+++ src/components/Search/Search.styles.js
@@ -5,6 +5,7 @@ export const SearchContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin: 0 .5rem;
 `;
 export const Headline = styled.h1`
   color: ${(props) => props.theme.color.dark};
