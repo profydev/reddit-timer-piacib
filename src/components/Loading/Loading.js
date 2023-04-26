8e004fc1feba4ef5af367790a6de510bced87480
piacib <ben.piacitelli@gmail.com>
2023-04-25 19:55:40 -0700

    formatting

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -17,9 +17,9 @@ const LoadingSVG = styled.img`
   animation: ${rotate360} 3s linear infinite;
   height: 20em;
   width:  20em;
-left:calc(50% - 10em);
-top:calc(50% - 10em + ${(props) => props.theme.table.tableHeadHeight} / 2);
-position: absolute;
+  left:calc(50% - 10em);
+  top:calc(50% - 10em + ${(props) => props.theme.table.tableHeadHeight} / 2);
+  position: absolute;
 `;
 const Loading = ({ isLoading }) => (isLoading ? <LoadingSVG src={loadingImg} alt="loading spinner" /> : null);
 Loading.propTypes = {
